8b497d9c840cca038f32dee01d43ea8b2cedfa5e
MustafoVohidi <vohidimustafo@gmail.com>
2022-03-30 23:57:03 +0300

    first commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use Illiminate\Support\Carbon;
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderBy('created_at', 'DESC')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newItem=new Item;
+        $newItem->name=$request->item['name'];
+        $newItem->save();
+        return $newItem;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $existingItenm=Item::find($id);
+        if($existingItenm){
+            $existingItenm->completed=$request->item['completed']?true:false;
+            $existingItenm->completed_at=$request->item['completed']?Carbon::now():null;
+            $existingItenm->save();
+            return $existingItenm;
+
+        }
+        return "Список не обнаружено";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $existingItenm=Item::find($id);
+        if($existingItenm){
+            $existingItenm->delete();
+            return "список успешно удален.";
+        }
+        return "Список не обнаружено";
+
+    }
+}
